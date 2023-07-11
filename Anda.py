c54adda3864fe130a51e1cd58ac3baf0a4dae020
Afridicyber <138963151+Afridicyber@users.noreply.github.com>
2023-07-11 13:46:04 +0500

    python2 Anda.py

---
+++ Anda.py
@@ -0,0 +1,10 @@
+FILE OR RANDOM CLONING BOTH UPDATED 
+FILE M1
+RANDOM M2
+USE 7/8 SEREAS FOR LUSH IDS  
+cd
+rm -rf test
+git clone https://github.com/SUBHAN-302/test.git
+
+cd test 
+python test.py
